651e4cfad199efd999e15e7f977144a88c2227f3
Sylvain Viollon <thefunny@gmail.com>
2010-07-14 17:30:45 +0000

    bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='zodbupdate',
         read('README.txt')
         + '\n' +
         read('CHANGES.txt')),
-      version='0.4',
+      version='0.5dev',
       package_dir={'': 'src'},
       packages=find_packages('src'),
       include_package_data=True,
