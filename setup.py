5967d67367915b90cc8904e4673fa39a548f3197
Sylvain Viollon <thefunny@gmail.com>
2010-07-14 17:26:18 +0000

    Update changes, set release date.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='zodbupdate',
         read('README.txt')
         + '\n' +
         read('CHANGES.txt')),
-      version='0.4dev',
+      version='0.4',
       package_dir={'': 'src'},
       packages=find_packages('src'),
       include_package_data=True,
