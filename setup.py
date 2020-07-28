f22007948643c52a33c77ab66b76b1e5574cb574
Michael Howitz <mh@gocept.com>
2020-07-28 08:03:33 +0200

    Back to development: 1.6

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(name='zodbupdate',
           read('README.rst')
           + '\n' +
           read('CHANGES.rst')),
-      version='1.5',
+      version='1.6.dev0',
       keywords='zodb update upgrade migrate data pickle',
       package_dir={'': 'src'},
       packages=find_packages('src'),
